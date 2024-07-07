036c50abae1144fb2a987308bcbf6e280be3f3a4
Bijay <bijaymahato031@gmail.com>
2024-07-08 03:27:28 +0545

    updated codes

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,13 +1,30 @@
 import express from "express";
 import { UserModel } from "../models/user.js";
 import { validationResult } from "express-validator";
+import bcrypt from "bcrypt";
 
-const Register = (req, res) => {
+const Register = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  return res.status(200).json("ok");
+  const { name, email, password } = req.body;
+  try {
+    const userExist = await UserModel.findOne({ email });
+    if (userExist) {
+      return res.status(400).json({
+        errors: [{ message: "User already exists" }],
+      });
+    }
+    const hashPassword = await bcrypt.hash(password, 12);
+    const newUser = new UserModel({ name, email, password: hashPassword });
+    const result = await newUser.save();
+    result._doc.password = undefined;
+    return res.status(201).json({ success: true, ...result._doc });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: err.message });
+  }
 };
 
 export { Register };
