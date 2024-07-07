4afc97592cee6e90cc355ea55ae0a8ca6ce25de8
Bijay <bijaymahato031@gmail.com>
2024-07-08 01:11:29 +0545

    configured dotenv

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config({ path: "./config/.env" });
 
-const PORT = 8000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`server is running at http://localhost:${PORT}`);
 });
