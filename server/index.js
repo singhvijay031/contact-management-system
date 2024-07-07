6bda6f31e3e546fe60d654280710074149dd1220
Bijay <bijaymahato031@gmail.com>
2024-07-08 01:22:44 +0545

    updated

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import "./config/db.js";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 dotenv.config({ path: "./config/.env" });
 
 const PORT = process.env.PORT;
