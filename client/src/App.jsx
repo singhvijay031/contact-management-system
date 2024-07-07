27b24852da9fe754f48851212e2cbef464da4ed4
Bijay <bijaymahato031@gmail.com>
2024-07-07 22:11:58 +0545

    modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
-import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <div>
-      <NavBar />
+      <Home />
     </div>
   );
 };
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+
+import NavBar from "../components/NavBar";
+
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <div className="home">
+        <h1 className="home-title">CONTACT MANAGEMENT SYSTEM</h1>
+        <p>
+          Start Collecting your contacs in a very smarter way. We provide very
+          efficeint and smarter way to handle contacts.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Home;
