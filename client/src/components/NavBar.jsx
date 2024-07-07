58e4dc5133680253a869339b26b28da8ede30d1c
Bijay <bijaymahato031@gmail.com>
2024-07-07 21:55:48 +0545

    added codes

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar-left">
+        <a href="" className="navbar-brand">
+          CONTACT MS
+        </a>
+      </div>
+      <div className="navbar-right">
+        <a href="" className="navbar-link">
+          About
+        </a>
+        <a href="" className="navbar-link">
+          Login
+        </a>
+        <a href="" className="navbar-link">
+          Register
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
