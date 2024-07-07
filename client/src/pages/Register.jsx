61bcd9a7b04168ff71d08be14189b15164616b83
Bijay <bijaymahato031@gmail.com>
2024-07-08 02:20:08 +0545

    updated

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -12,7 +12,7 @@ const Register = () => {
     password: "",
   });
   const [errors, setErrors] = useState({});
-  // const [serverErrors, setServerErrors] = useState([]);
+  const [serverErrors, setServerErrors] = useState([]);
 
   const handleInput = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
@@ -24,9 +24,8 @@ const Register = () => {
     setErrors(errs);
     if (errors.name === "" && errors.email === "" && errors.password === "") {
       axios
-        .post("", values)
+        .post("http://127.0.0.1:8000/ContactManagementSystem/register", values)
         .then((res) => {
-          console.log(res);
           toast.success("Account Created Successfully", {
             position: "top-right",
             autoClose: 5000,
