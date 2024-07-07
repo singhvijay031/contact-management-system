f851d4815553c1b31f772eac8ef72097bfb895e3
Bijay <bijaymahato031@gmail.com>
2024-07-08 01:18:00 +0545

    added db connection

---
+++ server/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: "./config/.env" });
+
+const Connection = async () => {
+  try {
+    await mongoose.connect(process.env.URI);
+    console.log("DataBase Connected...");
+  } catch (error) {
+    console.log("Cannot Connect to DataBase" + error.message);
+  }
+};
+
+Connection();
