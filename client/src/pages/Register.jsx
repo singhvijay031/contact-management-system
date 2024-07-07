c5413669356a9a892fb810bd601ef52fcb6b3073
Bijay <bijaymahato031@gmail.com>
2024-07-07 23:28:15 +0545

    made register form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,46 @@
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="form-container">
+      <form className="form">
+        <div className="form-group">
+          <label htmlFor="name" className="form-label">
+            Name:
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter Name"
+            name="name"
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email" className="form-label">
+            Email:
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+            name="email"
+            autoComplete="off"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password" className="form-label">
+            Password:
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="*******"
+            name="password"
+          />
+        </div>
+        <button className="form-btn">Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
