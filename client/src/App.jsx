08708568bb28e28ba55bb3a35d53a96fa3779dd6
Bijay <bijaymahato031@gmail.com>
2024-07-07 23:13:27 +0545

    added files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
+import Register from "./pages/Register";
 import Home from "./pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 const App = () => {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
