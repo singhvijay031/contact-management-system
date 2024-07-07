335a5331daa52a9a7d96bfa15be3418003bd1e7e
Bijay <bijaymahato031@gmail.com>
2024-07-08 00:42:43 +0545

    updated validations part

--- client/src/components/Validations.jsx
+++ client/src/components/Validations.jsx
@@ -1,32 +1,34 @@
 export default function Validations(values) {
   let errors = {};
 
-  const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/;
+  const email_pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const password_pattern =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
 
   if (values.name === "") {
-    errors.name = "Name should not be empty ";
-  } else if (values.name.length < 3 || values.name > 30) {
-    errors.name = "Name must be between 3-30";
+    errors.name = "Name should not be empty.";
+  } else if (values.name.length < 3 || values.name.length > 30) {
+    errors.name = "Name must be between 3 and 30 characters.";
   } else {
     errors.name = "";
   }
 
   if (values.email === "") {
-    errors.email = "Email should not be empty ";
+    errors.email = "Email should not be empty.";
   } else if (!email_pattern.test(values.email)) {
-    errors.email = "Invalid Email";
+    errors.email = "Invalid email address.";
   } else {
     errors.email = "";
   }
 
   if (values.password === "") {
-    errors.password = "Password should not be empty ";
+    errors.password = "Password should not be empty.";
   } else if (!password_pattern.test(values.password)) {
     errors.password =
-      "Password must contain one small letter one Capital letter and length should be greater than 8";
+      "Password must be at least 8 characters long and include at least one lowercase letter, one uppercase letter, one digit, and one special character (!@#$%^&*).";
   } else {
     errors.password = "";
   }
+
   return errors;
 }
