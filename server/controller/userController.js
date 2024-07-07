9fafd3fed9e035d6092cabdd676e947890967dd0
Bijay <bijaymahato031@gmail.com>
2024-07-08 01:58:53 +0545

    fixed errors

---
+++ server/controller/userController.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { UserModel } from "../models/user.js";
+
+const Register = () => {
+  console.log("register");
+};
+
+export { Register };
--- server/index.js
+++ server/index.js
@@ -2,13 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import "./config/db.js";
+import Router from "./routes/routes.js"; // Assuming Router is exported as default
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 dotenv.config({ path: "./config/.env" });
 
+app.use("/ContactManagementSystem", Router);
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
