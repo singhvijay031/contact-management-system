f2b07fdbd24c1dc20adb2c266edc8790f9cafc74
Bijay <bijaymahato031@gmail.com>
2024-07-10 00:59:52 +0545

    added register path

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,26 @@
 import express from "express";
 import { Register } from "../controller/userController.js";
 const router = express.Router();
+import { body } from "express-validator";
 
-router.post("/register", Register);
+router.post(
+  "/register",
+  [
+    body("name").trim().notEmpty().withMessage("Name Should Not Be Empty"),
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email Should Not Be Empty")
+      .isEmail()
+      .withMessage("Invalid Email"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("Passsword Should Not Be Empty ")
+      .isLength({ min: 5, max: 30 })
+      .withMessage("Password length Must Be Between 5-30"),
+  ],
+  Register
+);
 
 export { router as Router };
