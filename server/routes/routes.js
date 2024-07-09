b459ebabdba068c2605de754cafcef220259cc19
Bijay <bijaymahato031@gmail.com>
2024-07-10 00:05:46 +0545

    updated code

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,45 +1,4 @@
 import express from "express";
-import { Register, Login } from "../controller/userController.js";
 const router = express.Router();
-import { body } from "express-validator";
 
-router.post(
-  "/register",
-  [
-    (body("name").trim().notEmpty().withMessage("Name should not be empty"),
-    body("emal")
-      .trim()
-      .notEmpty()
-      .withMessage("Email should not be empty")
-      .isEmail()
-      .withMessage("Invalid Email..."),
-    body("password")
-      .trim()
-      .notEmpty()
-      .withMessage("Password should not be empty")
-      .isLength({ min: 8, max: 30 })
-      .withMessage("Password length should be 8-30 ")),
-  ],
-  Register
-);
-
-router.post(
-  "/login",
-  [
-    body("emal")
-      .trim()
-      .notEmpty()
-      .withMessage("Email should not be empty")
-      .isEmail()
-      .withMessage("Invalid Email..."),
-    body("password")
-      .trim()
-      .notEmpty()
-      .withMessage("Password should not be empty")
-      .isLength({ min: 8, max: 30 })
-      .withMessage("Password length should be 8-30 "),
-  ],
-  Login
-);
-
-export default router;
+router.post("/register");
