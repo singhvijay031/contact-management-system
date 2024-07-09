0746dda5f79f5502597bf64d5e73a151bd655bd3
Bijay <bijaymahato031@gmail.com>
2024-07-10 01:03:24 +0545

    added express-validator

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,8 +1,12 @@
+import { validationResult } from "express-validator";
 import { UserModel } from "../models/User.Model.js";
 
 const Register = (req, res) => {
-  console.log("Registered");
-  res.send("User Registered");
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  return res.status(200).json("OK");
 };
 
 export { Register };
