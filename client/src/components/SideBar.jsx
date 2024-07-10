7ad782f4f874aa2b70847fee6bd5bbc5b625a81a
Bijay <bijaymahato031@gmail.com>
2024-07-10 15:17:33 +0545

    added icons

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,7 +1,45 @@
 // import React from 'react'
+import { Link } from "react-router-dom";
+import {
+  FaCubesStacked,
+  FaUser,
+  FaAddressCard,
+  FaRegAddressCard,
+  FaPowerOff,
+} from "react-icons/fa6";
 
 const SideBar = () => {
-  return <div>Side Bar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar-item">
+        <FaCubesStacked className="top-icon" />
+      </div>
+      <div className="sidebar-item">
+        <Link to="/profile" className="sidebar-link">
+          <FaUser className="icon" />
+          Profile
+        </Link>
+      </div>
+      <div className="sidebar-item">
+        <Link to="/profile" className="sidebar-link">
+          <FaAddressCard className="icon" />
+          Contacts
+        </Link>
+      </div>
+      <div className="sidebar-item">
+        <Link to="/profile" className="sidebar-link">
+          <FaRegAddressCard className="icon" />
+          Add Contact
+        </Link>
+      </div>
+      <div className="sidebar-item">
+        <Link to="/profile" className="sidebar-link">
+          <FaPowerOff className="icon" />
+          Exit
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;
