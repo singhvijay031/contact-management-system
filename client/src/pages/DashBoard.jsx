6513da8dcc9b192884d2c9ce178035c09055ecd0
Bijay <bijaymahato031@gmail.com>
2024-07-10 14:29:31 +0545

    added css

--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -1,7 +1,22 @@
 // import React from 'react'
+import "../assets/css/DashBoard.css";
+
+import NavBar from "../components/NavBar";
 
 const DashBoard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <NavBar />
+      <div className="dashboard">
+        <div className="sidebar-container">
+          <h1>Side Bar</h1>
+        </div>
+        <div className="contact-container">
+          <h1>Content</h1>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DashBoard;
