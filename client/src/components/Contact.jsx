14d45bc11aea841a42557c3f98f4545a1ceb5bbb
Bijay <bijaymahato031@gmail.com>
2024-07-10 18:48:13 +0545

    updated css for icons

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -2,13 +2,39 @@
 
 import axios from "axios";
 import { useEffect, useState } from "react";
+import DataTable from "react-data-table-component";
+import { FaPenToSquare, FaRegTrashCan } from "react-icons/fa6";
 
 const Contact = () => {
   const [contacts, setContacts] = useState([]);
 
+  const columns = [
+    {
+      name: "Name",
+      selector: (row) => row.name,
+    },
+    {
+      name: "Email",
+      selector: (row) => row.email,
+    },
+    {
+      name: "Phone",
+      selector: (row) => row.phone,
+    },
+    {
+      name: "Action",
+      selector: (row) => (
+        <>
+          <FaPenToSquare className="table-icon1" />
+          <FaRegTrashCan className="table-icon2" />
+        </>
+      ),
+    },
+  ];
+
   useEffect(() => {
     axios
-      .get("http://127.0.0.1:8000/ContactManagementSystem/add-contact", {
+      .get("http://127.0.0.1:8000/ContactManagementSystem/contacts", {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
@@ -22,7 +48,11 @@ const Contact = () => {
         console.log(err);
       });
   }, []);
-  return <div>Contact</div>;
+  return (
+    <div className="contact-list">
+      <DataTable columns={columns} data={contacts} />
+    </div>
+  );
 };
 
 export default Contact;
