0aaf482b710e353ee691cb76d16db8f335246573
Bijay <bijaymahato031@gmail.com>
2024-07-10 19:13:21 +0545

    added routes for editing contact

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -4,9 +4,27 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
 import { FaPenToSquare, FaRegTrashCan } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+import CircleLoader from "react-spinners/CircleLoader";
+
+const customStyles = {
+  headCells: {
+    style: {
+      fontSize: 15 + "px",
+      fontWeight: 600,
+    },
+  },
+  cells: {
+    style: {
+      fontSize: 13 + "px",
+      fontWeight: 500,
+    },
+  },
+};
 
 const Contact = () => {
   const [contacts, setContacts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const columns = [
     {
@@ -25,14 +43,19 @@ const Contact = () => {
       name: "Action",
       selector: (row) => (
         <>
-          <FaPenToSquare className="table-icon1" />
-          <FaRegTrashCan className="table-icon2" />
+          <Link to={`/dashboard/edit-contact/${row._id}`}>
+            <FaPenToSquare className="table-icon1" />
+          </Link>
+          <Link>
+            <FaRegTrashCan className="table-icon2" />
+          </Link>
         </>
       ),
     },
   ];
 
   useEffect(() => {
+    setLoading(true);
     axios
       .get("http://127.0.0.1:8000/ContactManagementSystem/contacts", {
         headers: {
@@ -42,16 +65,37 @@ const Contact = () => {
       .then((res) => {
         if (res.data.success) {
           setContacts(res.data.contacts);
+          setLoading(false);
         }
       })
       .catch((err) => {
         console.log(err);
+        setLoading(false);
       });
   }, []);
   return (
-    <div className="contact-list">
-      <DataTable columns={columns} data={contacts} />
-    </div>
+    <>
+      {loading ? (
+        <div className="loader">
+          <CircleLoader
+            loading={loading}
+            size={50}
+            aria-label="Loading Spinner"
+            data-testide="loader"
+          />
+        </div>
+      ) : (
+        <div className="contact-list">
+          <DataTable
+            columns={columns}
+            data={contacts}
+            customStyles={customStyles}
+            pagination
+          />
+          {contacts.length === 0 ? <h1>Add a Contact</h1> : <></>}
+        </div>
+      )}
+    </>
   );
 };
 
