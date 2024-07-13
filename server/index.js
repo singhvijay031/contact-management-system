9aab8b82aaa13a7f5dc803e76decc3e5319660e5
Bijay <bijaymahato031@gmail.com>
2024-07-13 22:16:12 +0545

    updated codes

--- server/index.js
+++ server/index.js
@@ -4,20 +4,23 @@ import cors from "cors";
 import "./config/db.js";
 import { Router } from "./routes/routes.js";
 
-const app = express();
-app.use(express.json());
-app.use(
-  cors({
-    origin: ["https://contactms-client-nine.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
 dotenv.config({ path: "./config/.env" });
+
+const app = express();
+
+app.use(express.json());
+
+const corsOptions = {
+  origin: ["https://contactms-client-nine.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use("/ContactManagementSystem", Router);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
