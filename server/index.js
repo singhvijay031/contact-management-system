73171a57a4f98a3ed0a3e34eb50a01923d1baf69
Bijay <bijaymahato031@gmail.com>
2024-07-13 21:50:00 +0545

    updated code

--- server/index.js
+++ server/index.js
@@ -6,7 +6,13 @@ import { Router } from "./routes/routes.js";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://contactms-client-nine.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 dotenv.config({ path: "./config/.env" });
 
 app.use("/ContactManagementSystem", Router);
